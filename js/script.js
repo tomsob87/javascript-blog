5885473010e56011c0cd451b796faec93544a21b
Tomasz Sobczyk <tomasz.pawel.sobczyk@gmail.com>
2024-09-25 18:56:59 +0200

    Add class active for clickedElement

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 'use strict';
 
 function titleClickHandler(event){
+    event.preventDefault();
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
 
-    /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.post.active')
+    /* [DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.post.active');
 
     for(let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
