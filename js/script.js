dae2941fbe1401904883f38059deaa7d1efc419e
Tomasz Sobczyk <tomasz.pawel.sobczyk@gmail.com>
2024-09-26 15:19:25 +0200

    Add generateTitleLinks function

--- js/script.js
+++ js/script.js
@@ -32,9 +32,48 @@ function titleClickHandler(event){
     /* add class 'active' to the correct article */
     showArticle.classList.add('active');
   }
+
   
+  // Titles list generator
+
+const optArticleSelector = '.post',
+optTitleSelector = '.post-title',
+optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+  console.log("Clear titles sidebar section");
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+let html ='';
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  for(let article of articles){
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+    
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    /* get the title from the title element */
+    
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+    /* insert link into titleList */
+    html = html + linkHTML;
+    // titleList.innerHTML = titleList.innerHTML + linkHTML;
+    // console.log(html);
+  }
+  titleList.innerHTML = html;
+
   const links = document.querySelectorAll('.titles a');
   
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+}
+
+generateTitleLinks();
